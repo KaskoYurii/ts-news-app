d2878dd53ab340e47e66266491830ce2b21546e1
KaskoYurii <yuriy.kasko@quardex.com>
2025-04-14 19:16:17 +0300

    Update KEYs

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,6 @@ declare namespace NodeJS {
     NODE_ENV: string;
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
     VUE_ROUTER_BASE: string | undefined;
-    API_KEY: '67e515bb4f03009923f39c9dbe3567ffcf67ce9f88f9c937667d07f9554aa157';
+    API_KEY: string;
   }
 }
